16c36dd2a13c5eb6c6f45a58ea41d65494cee72a
Luca Clementi <luca.clementi@gmail.com>
2008-01-24 01:10:48 +0000

    New version of the web site with the new documentation, now the doucmentation is in docbook format.
    clem

---
+++ opal/opal-website/makedocbook.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# LC this file create the html and pdf of the docbook
+#
+#  0.1 first version
+
+
+VERSION=1.9.2
+# where sources file are located
+SOURCEDIR=src/docbook
+DSLFILE=$SOURCEDIR/etc
+# where the output will be placed
+DESTDIR=target/site/docs/$VERSION
+
+
+
+#
+# check the dest directory
+if [ ! -d $DESTDIR ] ;
+then 
+    mkdir -p $DESTDIR
+fi
+
+echo Generating the html version....
+jw --backend html --dsl $DSLFILE/rocks.dsl\#html --output $DESTDIR $SOURCEDIR/index.sgml
+
+echo Generating the pdf version....
+jw --backend pdf --dsl $DSLFILE/rocks.dsl\#print --output $DESTDIR $SOURCEDIR/index.sgml
+
+
+# finalizing the work...
+mv $DESTDIR/index.pdf $DESTDIR/opal-referenceguide.pdf
+cp -a $SOURCEDIR/images $DESTDIR
+cp $SOURCEDIR/rocks.css  $DESTDIR
+
+
