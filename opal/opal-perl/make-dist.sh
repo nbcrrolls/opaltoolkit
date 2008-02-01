1f8b94faf07b6120becd7532b14b321a1aeef31f
Wes Goodman <wgoodman@users.sourceforge.net>
2008-02-01 19:34:26 +0000

    Added README, make-dish.sh.  Changed docs to use OPAL-PERL instead of OPAL-PL.  Added comments to pdb2pqrclient.pl

---
+++ opal/opal-perl/make-dist.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VERSION=1.9.2
+
+echo "Creating a temporary directory for the tarball"
+rm -rf opal-perl-$VERSION
+mkdir opal-perl-$VERSION
+
+# copy dummy client, README, LICENSE, XSD, and WSDL into the temp directory
+echo "Copying sources into temporary directory"
+cp -r pdb2pqrclient.pl LICENSE README docs etc wsdl opal-perl-$VERSION/
+rm -rf `find opal-perl-$VERSION -name ".svn"`
+
+# create a tarball
+echo "Creating a tarball"
+tar zcvf opal-perl-$VERSION.tar.gz opal-perl-$VERSION/*
+
+# delete the temporary directory
+echo "Cleaning up"
+rm -rf opal-perl-$VERSION
