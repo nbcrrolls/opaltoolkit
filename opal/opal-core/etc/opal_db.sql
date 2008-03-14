cae404d24cfbdbe5efacac28d8a839e6206dd139
Luca Clementi <luca.clementi@gmail.com>
2008-03-14 00:00:12 +0000

    New schema for the opal DB, now it uses text so that we should not have anymore exception becuase the filename are too long
    clem

--- opal/opal-core/etc/opal_db.sql
+++ opal/opal-core/etc/opal_db.sql
@@ -2,7 +2,7 @@ create table job_status (
               job_id varchar(32) not null primary key,
        	      code integer not null,
        	      message text not null,
-       	      base_url varchar(128) not null,
+       	      base_url text not null,
 	      start_time varchar(128),
 	      last_update varchar(128),
        	      client_dn varchar(128),
@@ -11,12 +11,12 @@ create table job_status (
 
 create table job_output (
               job_id varchar(32) not null primary key,
-       	      std_out varchar(128) not null,
-       	      std_err varchar(128) not null,
+       	      std_out text not null,
+       	      std_err text not null,
        	      foreign key(job_id) references job_status(job_id));
 
 create table output_file (
               job_id varchar(32) not null,
-       	      name varchar(128) not null,
-       	      url varchar(128) not null,
+       	      name text not null,
+       	      url text not null,
        	      foreign key(job_id) references job_status(job_id));
