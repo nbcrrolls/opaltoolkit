495a207eb0080ffbd31f36dfcd2d8fe0c35db042
Sriram Krishnan <sriramkrishnan@gmail.com>
2008-04-15 00:29:27 +0000

    added a bunch of files to get hibernate to work. added a class to test addition of job status

--- etc/classpath.sh
+++ etc/classpath.sh
@@ -6,6 +6,6 @@ OPAL_HOME=$PWD
 # set the classpath
 CLASSPATH=`echo $OPAL_HOME/lib/*.jar | tr ' ' ':'`
 CLASSPATH=`echo $OPAL_HOME/webapps/opal2/WEB-INF/lib/*.jar | tr ' ' ':'`:$CLASSPATH
-CLASSPATH=$OPAL_HOME:$OPAL_HOME/build/classes:$OPAL_HOME/lib:$CLASSPATH
+CLASSPATH=$OPAL_HOME:$OPAL_HOME/build/classes:$OPAL_HOME/lib:$OPAL_HOME/etc:$CLASSPATH
 export CLASSPATH
 echo $CLASSPATH
