3b0862cdbc29b9770cd88a235201972b101eaa72
Sriram Krishnan <sriramkrishnan@gmail.com>
2008-04-15 18:35:01 +0000

    added another table to hibernate

--- opal/opal2-core/src/edu/sdsc/nbcr/opal/state/JobStatus.java
+++ opal/opal2-core/src/edu/sdsc/nbcr/opal/state/JobStatus.java
@@ -2,11 +2,6 @@ package edu.sdsc.nbcr.opal.state;
 
 import java.util.Date;
 
-import org.hibernate.Session;
-
-import java.util.List;
-import java.util.Iterator;
-
 import org.apache.log4j.Logger;
 
 /**
@@ -109,55 +104,5 @@ public class JobStatus {
 
     public void setServiceName(String serviceName) {
 	this.serviceName = serviceName;
-    }
-
-
-    // A simple main method to test functionality
-    public static void main(String[] args) 
-	throws Exception {
-
-	// initialize hibernate
-	logger.info("Initializing hibernate");
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
-	// initialize entry
-	JobStatus entry = new JobStatus();
-	entry.setJobID("app" + System.currentTimeMillis());
-	entry.setCode(0);
-	entry.setMessage("This is a test");
-	entry.setBaseURL("http://localhost/test");
-	entry.setStartTime(new Date());
-	entry.setLastUpdate(new Date());
-	entry.setClientDN("CN=Test");
-	entry.setClientIP("127.0.0.1");
-	entry.setServiceName("Command-line");
-
-	// save system entry
-	logger.info("Trying to save JobStatus into database");
-        session.beginTransaction();
-	session.save(entry);
-	session.getTransaction().commit();
-	session.close();
-	logger.info("Saved JobStatus into database successfully");
-
-	// list entries
-	logger.info("Listing JobStatus's");
-	session = HibernateUtil.getSessionFactory().openSession();
-	session.beginTransaction();
-	List results = session.createQuery("from JobStatus").list();
-	session.getTransaction().commit();
-	for (int i = 0; i < results.size(); i++) {
-	    entry = (JobStatus) results.get(i);
-	    logger.info("Job Status: " + entry.getJobID() +
-			" - {" + entry.getCode() +
-			", " + entry.getMessage() +
-			", " + entry.getBaseURL() +
-			", " + entry.getStartTime() + 
-			", " + entry.getLastUpdate() +
-			", " + entry.getClientIP() + 
-			", " + entry.getClientDN() + 
-			", " + entry.getServiceName() + "}");
-	}
-	session.close();
     }
 }
