e3dcacb9adb4f8b6a951ea251b5ef71de1de1df7
Sriram Krishnan <sriramkrishnan@gmail.com>
2009-03-05 02:11:01 +0000

    updated code to use reflection to instantiate job managers.

--- opal/opal2-core/src/edu/sdsc/nbcr/opal/manager/OpalJobManagerFactory.java
+++ opal/opal2-core/src/edu/sdsc/nbcr/opal/manager/OpalJobManagerFactory.java
@@ -1,31 +1,55 @@
 package edu.sdsc.nbcr.opal.manager;
 
-import edu.sdsc.nbcr.opal.JobManagerType;
-
-import edu.sdsc.nbcr.opal.manager.ForkJobManager;
-import edu.sdsc.nbcr.opal.manager.DRMAAJobManager;
-import edu.sdsc.nbcr.opal.manager.GlobusJobManager;
-import edu.sdsc.nbcr.opal.manager.RemoteGlobusJobManager;
-import edu.sdsc.nbcr.opal.manager.JobManagerException;
+import org.apache.log4j.Logger;
 
 /**
  * Factory class for creating instances of the Opal Job Manager
  */
 public class OpalJobManagerFactory {
 
-    public static OpalJobManager getOpalJobManager(JobManagerType jobManagerType) 
+    // get an instance of the log4j Logger
+    private static Logger logger = 
+	Logger.getLogger(OpalJobManagerFactory.class.getName());
+
+    public static OpalJobManager getOpalJobManager(String jobManagerFQCN) 
 	throws JobManagerException {
-	if (jobManagerType.equals(JobManagerType.drmaa)) {
-	    return new DRMAAJobManager();
-	} else if (jobManagerType.equals(JobManagerType.globusLocal)) {
-	    return new GlobusJobManager();
-	} else if (jobManagerType.equals(JobManagerType.globusRemote)) {
-	    return new RemoteGlobusJobManager();
-	} else if (jobManagerType.equals(JobManagerType.fork)) {
-	    return new ForkJobManager();
-	} else {
-	    throw new JobManagerException("Unknown JobManagerType: " +
-					  jobManagerType);
+
+	// check to see if the jobManagerFQCN is null
+	if (jobManagerFQCN == null) {
+	    throw new JobManagerException("Classname for job manager is null");
 	}
+
+	// instantiate a class object for this classname
+	Class managerClass = null;
+	try {
+	    managerClass = Class.forName(jobManagerFQCN);
+	} catch (ClassNotFoundException cnfe) {
+	    String msg = "Can't instantiate Opal Job Manager - " + 
+		"Job manager class " + jobManagerFQCN + " not found";
+	    logger.error(msg);
+	    throw new JobManagerException(msg);
+	}
+
+	// make sure that this class implements the correct interface
+	if (!OpalJobManager.class.isAssignableFrom(managerClass)) {
+	    String msg = "Can't instantiate Opal Job Manager - " + 
+		jobManagerFQCN + " does not implement the OpalJobManager interface";
+	    logger.error(msg);
+	    throw new JobManagerException(msg);
+	}
+
+	// instantiate the object
+	Object managerObject = null;
+	try {
+	    managerObject = managerClass.newInstance();
+	} catch (Exception e) {
+	    String msg = "Opal Job manager object can't be instantiated from class: " 
+		+ jobManagerFQCN;
+	    logger.error(msg);
+	    throw new JobManagerException(msg);
+	}
+
+	// return the manager class, typecast correctly
+	return (OpalJobManager) managerObject;
     }
 }
