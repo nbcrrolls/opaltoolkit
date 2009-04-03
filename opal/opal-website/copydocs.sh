3f2679ddf2253ef3ff1c8ea4fc7abe7a3bf31795
Luca Clementi <luca.clementi@gmail.com>
2009-04-03 21:46:52 +0000

    Update for the version 2.0 of Opal

--- opal/opal-website/copydocs.sh
+++ opal/opal-website/copydocs.sh
@@ -2,26 +2,22 @@
 
 VERSION=1.9
 
-
 echo ---
-echo Generating the documentation
+echo Generating the documentation for Opal $VERSION
 echo ---
 cd ../opal-core/
 ant -f build-admin.xml docs
 cd -
 
-
 echo ---
-echo Generating the Api Docuemtation
+echo Generating the Api Docuemtation for Opal $VERSION
 echo ---
 cd  ../opal-core/
 ant api-docs
 cd -
 
-
-
 echo ---
-echo Copying over everything
+echo Copying over everything -- Opal $VERSION --
 echo ---
 DESTDIR=target/site/docs/$VERSION/
 SRCDIR=../opal-core/docs/
@@ -35,3 +31,32 @@ cp ../opal-perl/docs/index.html $DESTDIR/opal-perl-index.html
 
 
 
+
+VERSION=2.0
+
+echo ---
+echo Generating the documentation for Opal $VERSION
+echo ---
+cd ../opal2-core/
+ant -f build-admin.xml docs
+cd -
+
+echo ---
+echo Generating the Api Docuemtation for Opal $VERSION
+echo ---
+cd  ../opal2-core/
+ant api-docs
+cd -
+
+echo ---
+echo Copying over everything -- Opal $VERSION --
+echo ---
+DESTDIR=target/site/docs/$VERSION/
+SRCDIR=../opal2-core/docs/
+# opal-core
+mkdir -p $DESTDIR
+cp -r $SRCDIR* $DESTDIR
+# opal-py
+cp ../opal-py/docs/index.html $DESTDIR/opal-py-index.html
+# opal-perl
+cp ../opal-perl/docs/index.html $DESTDIR/opal-perl-index.html
