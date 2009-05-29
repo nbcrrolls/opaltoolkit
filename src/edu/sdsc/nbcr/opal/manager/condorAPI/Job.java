96ed2f2f9c60549a1c792327f1b9a86bb9d5b247
Sriram Krishnan <sriramkrishnan@gmail.com>
2009-05-29 00:11:40 +0000

    updated package names, and added getStatus to the Job class

--- src/edu/sdsc/nbcr/opal/manager/condorAPI/Job.java
+++ src/edu/sdsc/nbcr/opal/manager/condorAPI/Job.java
@@ -1,6 +1,6 @@
-package condorAPI;
+package edu.sdsc.nbcr.opal.manager.condorAPI;
 
-import condorAPI.event.*;
+import edu.sdsc.nbcr.opal.manager.condorAPI.event.*;
 import java.util.*;
 
 /** 
@@ -9,214 +9,218 @@ import java.util.*;
  */ 
 
 public class Job implements Status, EventType {
-  /** 
-   * Job ID
-   */
-  JobId   jobId;
+    /** 
+     * Job ID
+     */
+    JobId   jobId;
 
-  /**
-   * Nodes in the Job
-   */
-  Node [] nodes;
+    /**
+     * Nodes in the Job
+     */
+    Node [] nodes;
 
-  /**
-   * Status of the job
-   */
-  int     status = UNEXPANDED;
+    /**
+     * Status of the job
+     */
+    int     status = UNEXPANDED;
 
-  /**
-   * Handlers for this job
-   */
-  HandlerSet handlers = new HandlerSet();
+    /**
+     * Handlers for this job
+     */
+    HandlerSet handlers = new HandlerSet();
 
-  /**
-   * The Condor this job belongs to.
-   */ 
-  Condor  condor;
+    /**
+     * The Condor this job belongs to.
+     */ 
+    Condor  condor;
 
-  /**
-   * A list of events the job got.
-   */
-  List eventList = new ArrayList();
+    /**
+     * A list of events the job got.
+     */
+    List eventList = new ArrayList();
 
-  /**
-   * Creates a Job object.
-   * @param jobId  jobID of the job
-   * @param condor the Condor
-   */
-  Job(JobId jobId, Condor condor){
+    /**
+     * Creates a Job object.
+     * @param jobId  jobID of the job
+     * @param condor the Condor
+     */
+    Job(JobId jobId, Condor condor){
 	this.jobId = jobId;
 	this.condor = condor;
-  }
+    }
 
-  /** 
-   * Returns a copy of the jobId
-   */
-  public JobId getJobId(){
+    /** 
+     * Returns a copy of the jobId
+     */
+    public JobId getJobId(){
 	return (JobId)(jobId.clone());
-  }
+    }
 
-  /**
-   * setup handlerset
-   */
-  void setHandlerSet(HandlerSet handlers){
+    /**
+     * setup handlerset
+     */
+    void setHandlerSet(HandlerSet handlers){
 	this.handlers = (HandlerSet)handlers.clone();
-  }
+    }
 
-  /**
-   * Waits for the job to be done.
-   */
-  public synchronized void waitFor() throws CondorException {
+    /**
+     * Waits for the job to be done.
+     */
+    public synchronized void waitFor() throws CondorException {
 	while (status == UNEXPANDED ||
-		   status == RUNNING || 
-		   status == IDLE    ||
-		   status == HELD    ){
-	  try { wait(); } catch (InterruptedException e){/* ignore */ }
+	       status == RUNNING || 
+	       status == IDLE    ||
+	       status == HELD    ){
+	    try { wait(); } catch (InterruptedException e){/* ignore */ }
 	}
-  }
+    }
 
-  /**
-   * Waits for the job to be running
-   */
-  public synchronized void waitForRun() throws CondorException {
+    /**
+     * Waits for the job to be running
+     */
+    public synchronized void waitForRun() throws CondorException {
 	while (status == UNEXPANDED ||
-		   status == IDLE    ||
-		   status == HELD    ){
-		try { 
-			wait(); 
-		} catch (InterruptedException e){/* ignore */ }
+	       status == IDLE    ||
+	       status == HELD    ){
+	    try { 
+		wait(); 
+	    } catch (InterruptedException e){/* ignore */ }
 	}
-  }
+    }
 
-  /**
-   * return if the job is held
-   */
-  public boolean isHeld() {
-	  return status == HELD;
-  }
+    /**
+     * return if the job is held
+     */
+    public boolean isHeld() {
+	return status == HELD;
+    }
 
-  /**
-   * return if the job is running
-   */
-  public boolean isRunning() {
-	  return status == RUNNING;
-  }
+    /**
+     * return if the job is running
+     */
+    public boolean isRunning() {
+	return status == RUNNING;
+    }
 
-  /**
-   * return if the job is idle
-   */
-  public boolean isIdle() {
-	  return status == IDLE;
-  }
+    /**
+     * return if the job is idle
+     */
+    public boolean isIdle() {
+	return status == IDLE;
+    }
 
-  /**
-   * return if the job is removed
-   */
-  public boolean isRemoved() {
-	  return status == REMOVED;
-  }
+    /**
+     * return if the job is removed
+     */
+    public boolean isRemoved() {
+	return status == REMOVED;
+    }
 
-  /**
-   * return if the job is completed
-   */
-  public boolean isCompleted() {
-	  return status == COMPLETED;
-  }
+    /**
+     * return if the job is completed
+     */
+    public boolean isCompleted() {
+	return status == COMPLETED;
+    }
   
   
-  /**
-   * Sets a handler that will be called on failure.
-   * @param handler - Handler to be invoked
-   */ 
-  public void setHandlerOnFailure(Handler handler){
+    /**
+     * Sets a handler that will be called on failure.
+     * @param handler - Handler to be invoked
+     */ 
+    public void setHandlerOnFailure(Handler handler){
 	handlers.setHandlerOnFailure(handler);
-  }
+    }
   
-  /**
-   * Sets a handler that will be called on success.
-   * @param handler - Handler to be invoked
-   */ 
-  public void setHandlerOnSuccess(Handler handler){
+    /**
+     * Sets a handler that will be called on success.
+     * @param handler - Handler to be invoked
+     */ 
+    public void setHandlerOnSuccess(Handler handler){
 	handlers.setHandlerOnSuccess(handler);
-  }
+    }
 
-  /**
-   * Sets a handler that will be called on a specified event.
-   * @param eventType - the Event Type
-   * @param handler   - Handler to be invoked
-   */ 
-  public void setHandler(int eventType, Handler handler){
+    /**
+     * Sets a handler that will be called on a specified event.
+     * @param eventType - the Event Type
+     * @param handler   - Handler to be invoked
+     */ 
+    public void setHandler(int eventType, Handler handler){
 	handlers.setHandler(eventType, handler);
-  }
+    }
 
 
 
-  /***************************************************
-   *              private functions                  *
-   ****************************************************/
+    /***************************************************
+     *              private functions                  *
+     ****************************************************/
 
- private void invokeCompletedHandler(Event e){
+    private void invokeCompletedHandler(Event e){
 	EventJobTerminated event = (EventJobTerminated)e;
 	if (event.returnValue == 0){
-	  Handler handler = handlers.getHandlerOnSuccess();
-	  if (handler != null)
+	    Handler handler = handlers.getHandlerOnSuccess();
+	    if (handler != null)
 		handler.handle(e);
 	} else {
-	  Handler handler = handlers.getHandlerOnFailure();
-	  if (handler != null)
+	    Handler handler = handlers.getHandlerOnFailure();
+	    if (handler != null)
 		handler.handle(e);
 	}
-  }
+    }
 
-  public String toString(){
+    public String toString(){
 	return jobId.toString() + " " + names[status];
-  }
+    }
 
 
-  synchronized void setStatus(int status, Event e){
+    synchronized void setStatus(int status, Event e){
 	if (this.status != status) {
-	  this.status = status;
-	  if (status == COMPLETED)
+	    this.status = status;
+	    if (status == COMPLETED)
 		condor.jobTerminated(this);
-	  if (e.type == JOB_TERMINATED)
+	    if (e.type == JOB_TERMINATED)
 		invokeCompletedHandler(e);
-	  notifyAll();
+	    notifyAll();
 	}
-  }
+    }
 
-  void event(Event e){
+    public int getStatus() {
+	return status;
+    }
+  
+    void event(Event e){
 	eventList.add(e);
 	switch (e.type){
 	case SUBMIT:
-	  setStatus(IDLE, e); break;
+	    setStatus(IDLE, e); break;
 	case EXECUTE:
-	  setStatus(RUNNING, e); break;
+	    setStatus(RUNNING, e); break;
 	case EXECUTABLE_ERROR:
-	  setStatus(IDLE, e); break;
+	    setStatus(IDLE, e); break;
 	case CHECKPOINTED:
-	  setStatus(IDLE, e); break;
+	    setStatus(IDLE, e); break;
 	case JOB_EVICTED:
-	  if (((EventJobEvicted)e).terminatedAndRequeued){
+	    if (((EventJobEvicted)e).terminatedAndRequeued){
 		setStatus(IDLE, e); break;
-	  } else {
+	    } else {
 		setStatus(REMOVED, e); break;
-	  }
+	    }
 	case JOB_TERMINATED:
-	  setStatus(COMPLETED, e); break;
+	    setStatus(COMPLETED, e); break;
 	case SHADOW_EXCEPTION:
-	  setStatus(IDLE, e); break;
+	    setStatus(IDLE, e); break;
 	case GENERIC:
-	  break;
+	    break;
 	case JOB_ABORTED:
-	  setStatus(COMPLETED, e); break;
+	    setStatus(COMPLETED, e); break;
 	case JOB_SUSPENDED:
-	  break;
+	    break;
 	case JOB_UNSUSPENDED:
-	  break;
+	    break;
 	case JOB_HELD:
-	  setStatus(HELD, e); break;
+	    setStatus(HELD, e); break;
 	case JOB_RELEASED:
-	  setStatus(IDLE, e); break;
+	    setStatus(IDLE, e); break;
 	case IMAGE_SIZE:
 	case NODE_EXECUTE:
 	case NODE_TERMINATED:
@@ -232,8 +236,8 @@ public class Job implements Status, EventType {
 	}
 	Handler handler = handlers.getHandler(e.type);
 	if (handler != null)
-	  handler.handle(e);
+	    handler.handle(e);
 
-  }
+    }
 }
 
