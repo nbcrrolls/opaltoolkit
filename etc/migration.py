c278c187535322c64f81716a2053d81dad653eab
Luca Clementi <luca.clementi@gmail.com>
2009-05-29 19:28:30 +0000

    Some update on the migration script
    clem

--- etc/migration.py
+++ etc/migration.py
@@ -11,18 +11,19 @@ import pg
 import sys
 
 #changeME
-db_new = pg.connect (dbname="opal2_db", user="opal_user", passwd="opal_passwd", host='localhost')
-db_old = pg.connect (dbname="opal_db", user="opal_user", passwd="opal_passwd", host='localhost')
+db_new = pg.connect (dbname="opal2_db", user="opal_user", passwd="opal_passwd", host='localhost', port=5433)
+db_old = pg.connect (dbname="opal_db", user="opal_user", passwd="opal_passwd", host='localhost', port=5433)
 startoutputID=60
 startoutfilesID=60
 #end changeme
 
 old_job_status = db_old.query("select * from job_status")
+counter=0
 for entry in old_job_status.dictresult():
     #let get all the data we need 
     job_id = entry["job_id"]
-    job_outputs = db_old.query("select * from job_output where job_id = \'%s\'" % job_id)
-    output_files = db_old.query("select * from output_file where job_id = \'%s\'" % job_id)
+    #job_outputs = db_old.query("select * from job_output where job_id = \'%s\'" % job_id)
+    #output_files = db_old.query("select * from output_file where job_id = \'%s\'" % job_id)
 
     #we have to parse starttime and lastupdate
     import time, datetime
@@ -41,12 +42,26 @@ for entry in old_job_status.dictresult():
         #print "insert failed! Aborting"
         #sys.exit(-1)
     #second we load the job_outputs table 
-    for output in job_outputs.dictresult():
-        db_new.query("insert into job_output (id, job_id, std_out, std_err) values ("+str(startoutputID)+", \'"+job_id+"\', \'"+output['std_out']+\
-            "\', \'"+output['std_err']+"\')")
-        startoutputID=startoutputID+1
-    for file in output_files.dictresult():
-        db_new.query("insert into output_file (id, url, name, job_id) values ("+str(startoutfilesID)+", \'"+file['url']+"\', \'"+file['name']+"\', \'"+job_id+"\')")
-        startoutfilesID=startoutfilesID+1
+    #if you are migrating you probably don't need to bring this in the new DB
+    #for output in job_outputs.dictresult():
+        #db_new.query("insert into job_output (id, job_id, std_out, std_err) values ("+str(startoutputID)+", \'"+job_id+"\', \'"+output['std_out']+\
+        #    "\', \'"+output['std_err']+"\')")
+        #startoutputID=startoutputID+1
+    #for file in output_files.dictresult():
+        #db_new.query("insert into output_file (id, url, name, job_id) values ("+str(startoutfilesID)+", \'"+file['url']+"\', \'"+file['name']+"\', \'"+job_id+"\')")
+        #startoutfilesID=startoutfilesID+1
+    #let's get some progress output
+    counter=counter+1
+    if counter%5 == 0:
+        print "" + str(counter) + " inserted"
+
+
+
+
+
+
+
+
+
 
 
