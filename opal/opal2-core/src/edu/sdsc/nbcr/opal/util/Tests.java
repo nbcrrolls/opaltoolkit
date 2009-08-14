a64d8cc36312ce08ac2a1968c63d33984fc822c1
Luca Clementi <luca.clementi@gmail.com>
2009-08-14 22:11:21 +0000

    During tests we are now using a in memory database.

--- opal/opal2-core/src/edu/sdsc/nbcr/opal/util/Tests.java
+++ opal/opal2-core/src/edu/sdsc/nbcr/opal/util/Tests.java
@@ -3,6 +3,11 @@ package edu.sdsc.nbcr.opal.util;
 import junit.framework.TestSuite;
 import junit.framework.Test;
 
+import edu.sdsc.nbcr.opal.state.HibernateUtil;
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+
+
 /**
  * TestSuite that runs all the Opal tests
  *
@@ -10,18 +15,29 @@ import junit.framework.Test;
  */
 public class Tests {
 
-  public static void main(String[] args) {
-    junit.textui.TestRunner.run(suite());
-    System.exit(0);
-  }
+    // get an instance of the log4j Logger
+    private static Logger logger = Logger.getLogger(Test.class.getName());
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+        System.exit(0);
+    }
   
-  public static Test suite ( ) {
-    TestSuite suite= new TestSuite("All JUnit Tests for Opal2");
+    public static void setUp() {
+        // initialize hibernate
+        logger.info("Initializing hibernate");
+        HibernateUtil.setConfFile("hibernate-opal.cfg.tests.xml");
+        SessionFactory session = HibernateUtil.getSessionFactory();
+    }
 
-    suite.addTest(edu.sdsc.nbcr.opal.state.PackageTest.suite());
-    suite.addTest(edu.sdsc.nbcr.opal.util.PackageTest.suite());
-    // suite.addTest(edu.sdsc.nbcr.opal.dashboard.util.PackageTest.suite());
 
-    return suite;
-  }
+    public static Test suite ( ) {
+        TestSuite suite= new TestSuite("All JUnit Tests for Opal2");
+        setUp();
+        suite.addTest(edu.sdsc.nbcr.opal.state.PackageTest.suite());
+        suite.addTest(edu.sdsc.nbcr.opal.util.PackageTest.suite());
+        suite.addTest(edu.sdsc.nbcr.opal.dashboard.util.PackageTest.suite());
+
+        return suite;
+    }
 }
