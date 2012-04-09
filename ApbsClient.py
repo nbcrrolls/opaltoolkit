413435bb82ce1cde2ccebf349b7187a85762fa43
Luca Clementi <luca.clementi@gmail.com>
2012-04-09 23:54:09 +0000

    Now the Apbs Client uses OpalClient interface

--- ApbsClient.py
+++ ApbsClient.py
@@ -5,87 +5,35 @@
 import sys
 import time
 import httplib
+import OpalClient
 
-from AppService_client import \
-     AppServiceLocator, getAppMetadataRequest, launchJobRequest, \
-     queryStatusRequest, getOutputsRequest, \
-     launchJobBlockingRequest, getOutputAsBase64ByNameRequest
-from AppService_types import ns0
-from ZSI.TC import String
 
-# Set the protocol to http or https
-proto = "http"
-
-# Host and port for remote services
-baseURL = proto + "://ws.nbcr.net/"
-
-# Retrieve a reference to the AppServicePort
-appLocator = AppServiceLocator()
-appServicePort = appLocator.getAppServicePort(
-    baseURL + "opal2/services/APBS_1.1.0")
-	
-# Set up remote job launch
-req = launchJobRequest()
+# Service URL
+apbsURL = "http://kryptonite.nbcr.net/opal2/services/apbs_1.3"
+client = OpalClient.OpalService(apbsURL)
 
 # command-line arguments
-req._argList = "apbs.in"
+argList = "apbs.in"
+inputFile = ["etc/apbs.in", "etc/ion.xml"]
 
-# append all input files in this manner - in this case we have two of them
-inputFiles = []
-inputFile0 = ns0.InputFileType_Def('inputFile')
-inputFile0._name = 'apbs.in'
-#sampleFile0 = open("etc/apbs.in", "r")
-#inputFile0._contents = sampleFile0.read()
-#sampleFile0.close()
-#To send an attachment use the _attachment instead of the _contents
-inputFile0._attachment = open("etc/apbs.in", "r")
-inputFiles.append(inputFile0)
-
-inputFile1 = ns0.InputFileType_Def('inputFile')
-inputFile1._name = 'ion.xml'
-#sampleFile1 = open("etc/ion.xml", "r")
-#inputFile1._contents = sampleFile1.read()
-#sampleFile1.close()
-#To send attachment use the _attachment instead of the _contents
-inputFile1._attachment = open("etc/ion.xml", "r")
-inputFiles.append(inputFile1)
-
-req._inputFile = inputFiles
-
-# Launch job, and retrieve job ID
 print "Launching remote Apbs job"
-resp = appServicePort.launchJob(req)
-jobID = resp._jobID
-print "Received Job ID:", jobID
-
-# Poll for job status
-status = resp._status
-print "Polling job status"
-while 1:
-    # print current status
-    print "Status:"
-    print "\tCode:", status._code
-    print "\tMessage:", status._message
-    print "\tOutput Base URL:", status._baseURL
-
-    if (status._code == 8) or (status._code == 4): # STATUS_DONE || STATUS_FAILED
-        break
-
-    # Sleep for 30 seconds
-    print "Waiting 30 seconds"
-    time.sleep(30)
+jobStatus = client.launchJobNB(argList, inputFile)
+while jobStatus.isRunning() :
+    time.sleep(3)
+    print "Polling job status"
+    jobStatus.updateStatus()
     
-    # Query job status
-    status = appServicePort.queryStatus(queryStatusRequest(jobID))
+if jobStatus.isSuccessful():
+    print "Job execution finished sucessfully."
+else:
+    print "Job execution failed."
+    print "Check the stdout.txt and the stderr.txt for errors...."
 
-# Retrieve job outputs, if execution is successful
-if status._code == 8: # 8 = GramJob.STATUS_DONE
-    print "Retrieving Apbs output metadata: "
-    resp = appServicePort.getOutputs(getOutputsRequest(jobID))
+print "Job URL: ", jobStatus.getBaseURL()
+# Retrieve a listing of all output files
+print "\tStandard Output:", jobStatus.getURLstdout(), "\n", \
+  "\tStandard Error:", jobStatus.getURLstderr()
 
-    # Retrieve a listing of all output files
-    print "\tStandard Output:", resp._stdOut, "\n", \
-	  "\tStandard Error:", resp._stdErr
-    if (resp._outputFile != None):
-	for i in range(0, resp._outputFile.__len__()):
-	    print "\t" + resp._outputFile[i]._name, ":", resp._outputFile[i]._url
+files = jobStatus.getOutputFiles()
+for i in files: 
+    print "\t" + i
