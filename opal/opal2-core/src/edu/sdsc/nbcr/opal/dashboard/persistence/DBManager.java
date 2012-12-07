1207b92f84b0df3af2c50088429c85dc3fa9fa1f
Luca Clementi <luca.clementi@gmail.com>
2012-12-07 20:51:23 +0000

    remove the last installment of "HSQL doesn't support ACTIVE and INACTIVE services"

--- opal/opal2-core/src/edu/sdsc/nbcr/opal/dashboard/persistence/DBManager.java
+++ opal/opal2-core/src/edu/sdsc/nbcr/opal/dashboard/persistence/DBManager.java
@@ -160,20 +160,11 @@ public class DBManager {
         Session session = sessionFactory.openSession();
         List serviceList = null;
 
-        if (dialect.equals("org.hibernate.dialect.HSQLDialect")){
-	    // HSQL doesn't support ACTIVE and INACTIVE services
-	    // so select every service that has been in the DB
-	    serviceList = 
-		session.createQuery("select serviceName from JobInfo " + 
-				    "group by serviceName ").list();
-
-	} else {
 	    // select only ACTIVE services
 	    serviceList = 
 		session.createQuery("select serviceName from ServiceStatus " +
 				    "where status='ACTIVE' group by serviceName ").
 		list();
-	}
         session.close();
         Iterator itera = serviceList.iterator();
         HashSet returnList = new HashSet();
